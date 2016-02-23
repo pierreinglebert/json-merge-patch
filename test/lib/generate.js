518b6a1ad22375efa2f2505d2f8184fcdd5d375f
Adrien Risser <risseraka@gmail.com>
2016-02-23 09:36:19 -0500

    fixing indent, replacing tabs with spaces

--- test/lib/generate.js
+++ test/lib/generate.js
@@ -5,123 +5,123 @@ var assert = require('chai').assert;
 var generate = require('../../lib/generate');
 
 describe('generate', function() {
-	it('should generate a patch replacing an attribute', function() {
-		assert.deepEqual(
-			generate({a: 'b'}, {a: 'c'}),
-	    {a: 'c'}
+  it('should generate a patch replacing an attribute', function() {
+    assert.deepEqual(
+      generate({a: 'b'}, {a: 'c'}),
+      {a: 'c'}
     );
-	});
+  });
 
-	it('should generate a patch adding an attribute', function() {
-		assert.deepEqual(
-			generate({a: 'b'}, { a: 'b', b: 'c'}),
-			{b: 'c'}
+  it('should generate a patch adding an attribute', function() {
+    assert.deepEqual(
+      generate({a: 'b'}, { a: 'b', b: 'c'}),
+      {b: 'c'}
     );
-	});
+  });
 
-	it('should generate a patch deleting an attribute', function() {
-		assert.deepEqual(
-			generate({a: 'b'}, {}),
-			{a: null}
+  it('should generate a patch deleting an attribute', function() {
+    assert.deepEqual(
+      generate({a: 'b'}, {}),
+      {a: null}
     );
-	});
+  });
 
-	it('should generate a patch deleting an attribute without affecting others', function() {
-		assert.deepEqual(
-			generate({a: 'b', b: 'c'}, {b: 'c'}),
-			{a: null}
-		);
-	});
+  it('should generate a patch deleting an attribute without affecting others', function() {
+    assert.deepEqual(
+      generate({a: 'b', b: 'c'}, {b: 'c'}),
+      {a: null}
+    );
+  });
 
-	it('should generate a patch replacing an attribute if its an array', function() {
-		assert.deepEqual(
-			generate({a: ['b']}, {a: 'c'}),
-			{a: 'c'}
-		);
-	});
+  it('should generate a patch replacing an attribute if its an array', function() {
+    assert.deepEqual(
+      generate({a: ['b']}, {a: 'c'}),
+      {a: 'c'}
+    );
+  });
 
-	it('should generate a patch replacing the attribute with an array', function() {
-		assert.deepEqual(
-			generate({a: 'c'}, {a: ['b']}),
-			{a: ['b']}
-		);
-	});
+  it('should generate a patch replacing the attribute with an array', function() {
+    assert.deepEqual(
+      generate({a: 'c'}, {a: ['b']}),
+      {a: ['b']}
+    );
+  });
 
-	it('should generate a patch replacing an object array with a number array', function() {
-		assert.deepEqual(
-			generate({a: [{b: 'c'}]}, {a: [1]}),
-			{a: [1]}
-		);
-	});
+  it('should generate a patch replacing an object array with a number array', function() {
+    assert.deepEqual(
+      generate({a: [{b: 'c'}]}, {a: [1]}),
+      {a: [1]}
+    );
+  });
 
-	it('should generate a patch replacing whole array if one element has changed', function() {
-		assert.deepEqual(
-			generate(['a', 'b'], ['c', 'd']),
-			['c', 'd']
-		);
-	});
+  it('should generate a patch replacing whole array if one element has changed', function() {
+    assert.deepEqual(
+      generate(['a', 'b'], ['c', 'd']),
+      ['c', 'd']
+    );
+  });
 
-	it('should generate a patch replacing whole array if one element has been deleted', function() {
-		assert.deepEqual(
-			generate(['a', 'b'], ['a']),
-			['a']
-		);
-	});
+  it('should generate a patch replacing whole array if one element has been deleted', function() {
+    assert.deepEqual(
+      generate(['a', 'b'], ['a']),
+      ['a']
+    );
+  });
 
-	it('should generate a patch replacing with an array', function() {
-		assert.deepEqual(
-			generate({a: 'b'}, ['c']),
-			['c']
-		);
-	});
+  it('should generate a patch replacing with an array', function() {
+    assert.deepEqual(
+      generate({a: 'b'}, ['c']),
+      ['c']
+    );
+  });
 
-	it('should generate a patch replacing with null', function() {
-		assert.deepEqual(
-			generate({a: 'foo'}, null),
-			null
-		);
-	});
+  it('should generate a patch replacing with null', function() {
+    assert.deepEqual(
+      generate({a: 'foo'}, null),
+      null
+    );
+  });
 
-	it('should generate a patch replacing with a string', function() {
-		assert.deepEqual(
-			generate({a: 'foo'}, 'bar'),
-			'bar'
-		);
-	});
+  it('should generate a patch replacing with a string', function() {
+    assert.deepEqual(
+      generate({a: 'foo'}, 'bar'),
+      'bar'
+    );
+  });
 
-	it('should generate a patch keeping null attributes', function() {
-		assert.deepEqual(
-			generate({e: null}, {e: null, a: 1}),
-			{a: 1}
-		);
-	});
+  it('should generate a patch keeping null attributes', function() {
+    assert.deepEqual(
+      generate({e: null}, {e: null, a: 1}),
+      {a: 1}
+    );
+  });
 
-	it('should work recursively', function() {
-		assert.deepEqual(
-			generate({}, {a: {bb: {}}}),
-			{a: {bb: {}}}
-		);
-	});
+  it('should work recursively', function() {
+    assert.deepEqual(
+      generate({}, {a: {bb: {}}}),
+      {a: {bb: {}}}
+    );
+  });
 
-	it('should return undefined if the object hasnt changed', function() {
-		assert.deepEqual(
-			generate({a: 'a'}, {a: 'a'}),
-			undefined
-		);
-	});
+  it('should return undefined if the object hasnt changed', function() {
+    assert.deepEqual(
+      generate({a: 'a'}, {a: 'a'}),
+      undefined
+    );
+  });
 
-	it('should return undefined if the object with sub attributes hasnt changed', function() {
-		assert.deepEqual(
-			generate({a: {b: 'c'}}, {a: {b: 'c'}}),
-			undefined
-		);
-	});
+  it('should return undefined if the object with sub attributes hasnt changed', function() {
+    assert.deepEqual(
+      generate({a: {b: 'c'}}, {a: {b: 'c'}}),
+      undefined
+    );
+  });
 
-	it('should return undefined if the array hasnt changed', function() {
-		assert.deepEqual(
-			generate([1,2,3], [1,2,3]),
-			undefined
-		);
-	});
+  it('should return undefined if the array hasnt changed', function() {
+    assert.deepEqual(
+      generate([1,2,3], [1,2,3]),
+      undefined
+    );
+  });
 
 });
